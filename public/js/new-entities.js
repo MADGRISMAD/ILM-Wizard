8a6b9c4bd1408f3b81e9627725bf2a95d970502d
madeline <madgrismad@gmail.com>
2023-08-29 10:21:24 -0700

    ahh

--- public/js/new-entities.js
+++ public/js/new-entities.js
@@ -1,11 +1,26 @@
+// $(document).ready (function () {
+//   $.get('http://localhost:3000/newentities/obtenerEntidades', function (data, status) {
+//     console.log(data);
+//     console.log(status);
+//   }
+//   );
+// });
 
+const NewEntities = {
 
-
-$(document).ready (function () {
-  $.get('http://localhost:3000/newentities/obtenerEntidades', function (data, status) {
-    console.log(data);
-    console.log(status);
+  getEntitites: function () {
+     return $.get('http://localhost:3000/newentities/obtenerEntidades', function (data, status) { });
   }
-  );
-});
+}
 
+
+  // $(document).ready(function(){
+
+  //   $("#country1").append(entidades[0].identifier);
+  //   $("#country2").append(entidades[1].identifier);
+
+  //   $("#region1").append(companies[0].Company);
+  //   $("#region2").append(companies[1].Company);
+
+
+  // });
