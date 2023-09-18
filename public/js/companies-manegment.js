a639735f6097bd719a37981d469638da5c687bbb
madeline <madgrismad@gmail.com>
2023-09-18 01:09:40 -0700

    modify

--- public/js/companies-manegment.js
+++ public/js/companies-manegment.js
@@ -27,6 +27,8 @@ $(document).ready(function () {
   // Llama a la función para obtener y mostrar las compañías
   obtenerYMostrarCompanies(selectedCompanyName);
 
+
+
   // Función para obtener y mostrar las compañías
   function obtenerYMostrarCompanies(selectedCompanyName) {
     // Realiza una solicitud AJAX para obtener los datos de las compañías
@@ -103,9 +105,72 @@ $(document).ready(function () {
 
   function actualizarTarjeta(entidades) {
    //actualizar el logo de la entidad
-    $(".card-img-top").attr("src", entidades.flag);
+    $(".card-img-top.current-company").attr("src", entidades.flag);
     //ACTUALIZAR EL NOMBRE DE LA ENTIDAD
     $("#company-name").text(entidades.companyName);
 
   }
+});
+
+
+$(document).ready(function () {
+
+  // Función para crear el modal de agregar compañía
+  function crearModalAgregarCompania() {
+      // Estructura básica del modal
+      let modalHTML = `
+          <div class="modal fade" id="modalAgregarCompania" tabindex="-1" aria-labelledby="modalAgregarCompaniaLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title" id="modalAgregarCompaniaLabel">Agregar Nueva Compañía</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                          <form id="formAgregarCompania">
+                              <div class="mb-3">
+                                  <label for="companyName" class="form-label">Nombre de la Compañía</label>
+                                  <input type="text" class="form-control" id="companyName" required>
+                              </div>
+                          </form>
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                          <button type="submit" form="formAgregarCompania" class="btn btn-primary">Guardar</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      `;
+
+      // Agregamos el modal al cuerpo del documento
+      $('body').append(modalHTML);
+
+      // Evento cuando se envía el formulario del modal
+      $('#formAgregarCompania').on('submit', function (e) {
+          e.preventDefault();
+
+          // Aquí puedes agregar el código para manejar la información del formulario,
+          // por ejemplo, hacer una solicitud AJAX al servidor para guardar la nueva compañía
+
+          const companyName = $('#companyName').val();
+          console.log('Nombre de la compañía a guardar:', companyName);
+
+          // Después de manejar el formulario, cierra el modal
+          var modalAgregarCompania = new bootstrap.Modal(document.getElementById('modalAgregarCompania'));
+          modalAgregarCompania.hide();
+      });
+  }
+
+  // Invocamos la función para crear el modal
+  crearModalAgregarCompania();
+
+  // Evento para abrir el modal cuando se haga clic en el botón de "Agregar Nueva Compañía"
+  $('#agregarCompania').click(function () {
+      var modalAgregarCompania = new bootstrap.Modal(document.getElementById('modalAgregarCompania'));
+      modalAgregarCompania.show();
+  });
+
+  // Aquí puedes continuar con el resto del código que maneja las empresas...
+  // Por ejemplo, el código que maneja obtenerYMostrarCompanies...
 });
